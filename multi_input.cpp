714121696f9722b3d06cf92a4d6c1d2b76b950ef
Volodymir Konovalov <vvkon@meta.ua>
2019-10-17 06:43:54 +0300

    Update multi_input.cpp

--- multi_input.cpp
+++ multi_input.cpp
@@ -5,5 +5,9 @@ int main()
     cout << "Enter several octal, decimal, \n"
         << "and hexadecimal integers in any combination \n"
         << "using the suffixes 0 and 0x:\n";
+    string str_num;
+    cin >> str_num;
+    vector<int> nums;
+
     return 0;
 }
