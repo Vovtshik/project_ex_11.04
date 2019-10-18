9aa8f22d3c8d9b643032ac9d8d9a071f07b70e4f
Volodymir Konovalov <vvkon@meta.ua>
2019-10-18 06:35:32 +0300

    Update multi_input.cpp

--- multi_input.cpp
+++ multi_input.cpp
@@ -1,6 +1,6 @@
 #include "../std_lib_facilities.h"
 
-void convert_to_decimal(string& s, vector<int>& vi);
+void convert_to_decimal(string& s);
 
 int main()
 {
@@ -14,14 +14,43 @@ int main()
     return 0;
 }
 
-void convert_to_decimal(string& s, vector<int>& vi)
+void convert_to_decimal(string& s)
 {
     istringstream is(s);
-    is.unsetf(ios::dec);
-    is.unsetf(ios::oct);
-    is.unsetf(ios::hex);
-    for(int temp; is >> temp;)
+    char ch_t;
+    is >> ch_t;
+    if(ch_t == '0')
+        {
+            is >> ch_t;
+            if(ch_t == 'x')
+                {
+                    string temp;
+                    is >> temp;
+                    temp = "0x" + temp;
+                    cout << showbase << hex << temp << "(hexadecimal)  turns into decimal " 
+                    << dec << temp << noshowbase << '\n';
+                }
+            else
+                {
+                    is.unget();
+                    string temp;
+                    temp = '0' + temp;
+                    cout << showbase << oct << temp << "(octal) turns into decimal" 
+                    << oct << temp << noshowbase << '\n';
+                }
+        }
+    else
     {
-        vi.push_back(temp);
+        is.unget();
+        string temp;
+        cout << temp << "(decimal) turns into decimal" << temp << noshowbase << '\n';
     }
+    // is.unsetf(ios::dec);
+    // is.unsetf(ios::oct);
+    // is.unsetf(ios::hex);
+    // for(int temp; is >> temp;)
+    // {
+    //     vi.push_back(temp);
+    // }
+
 }
