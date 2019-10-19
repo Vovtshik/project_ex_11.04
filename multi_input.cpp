da08e4541d575254aa707e9a4d53b95c6dfd732b
Volodymir Konovalov <vvkon@meta.ua>
2019-10-19 06:29:26 +0300

    Update multi_input.cpp

--- multi_input.cpp
+++ multi_input.cpp
@@ -17,8 +17,6 @@ int main()
 void convert_to_decimal(string& s)
 {
     istringstream is(s);
-    // char ch_t;
-    // is >> ch_t;
     for(string str; is >> str;)
     {
         istringstream ist(str);
@@ -56,47 +54,4 @@ void convert_to_decimal(string& s)
             cout << temp << "(decimal) turns into decimal" << temp << '\n';
         }
     }
-    // for(char ch_t; is >> ch_t;)
-    // {
-    //     if(ch_t == '0')
-    //     {
-    //         is >> ch_t;
-    //         if(ch_t == 'x')
-    //             {
-    //                 string str_t;
-    //                 is >> str_t;
-    //                 str_t = "0x" + str_t;
-    //                 istringstream ist(str_t);
-    //                 int temp;
-    //                 ist >> temp;
-    //                 cout << showbase << hex << temp << " (hexadecimal)  turns into decimal " 
-    //                 << dec << temp << noshowbase << '\n';
-    //             }
-    //         else
-    //             {
-    //                 is.unget();
-    //                 int temp;
-    //                 is >> temp;
-    //                 cout << showbase << oct << temp << "(octal) turns into decimal" 
-    //                 << dec << temp << noshowbase << '\n';
-    //             }
-    //     }
-    //     else
-    //     {
-    //         is.unget();
-    //         int temp;
-    //         is >> temp;
-    //         cout << temp << "(decimal) turns into decimal" << temp << '\n';
-    //     }
-    
-    // }
-    
-    // is.unsetf(ios::dec);
-    // is.unsetf(ios::oct);
-    // is.unsetf(ios::hex);
-    // for(int temp; is >> temp;)
-    // {
-    //     vi.push_back(temp);
-    // }
-
 }
